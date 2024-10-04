e5d61357f40319e10ee99b530b7f49ecca34bf71
medina-matthew <medina.matthew1030@GMAIL.Com>
2024-10-04 08:36:52 -0700

    Update password-strength-tester.py
    Fixing the input validation not correctly being called. Moving it directly into the main function, removing the if statement with the calling of class.

--- password-strength-tester.py
+++ password-strength-tester.py
@@ -1,18 +1,22 @@
 from password_checker import PasswordChecker
 
+
+# Defining main, calls class PasswordChecker
+# Input validation to make sure string is not emptys
+
 def main():
     print("Hello, welcome to the password strength tester!\n")
 
     password = input("Enter a password to test its strength:\n")
+
+    if input == '':
+        print('Password cannot be empty')
                      
     checker = PasswordChecker(password)
 
-    if checker.input_validation:
-        print('Password cannot be empty\n')
-    else:
-        result = checker.check_strength()
+    result = checker.check_strength()
 
-        print(result)
+    print(result)
 
 if __name__ == "__main__":
     main()
