7036a3a2b3acfd4fce6bd88eb2082fc4f7da121a
medina-matthew <medina.matthew1030@GMAIL.Com>
2024-10-04 09:59:15 -0700

    Updating input validation
    Finished the input validation, also reformatted file.

--- password-strength-tester.py
+++ password-strength-tester.py
@@ -4,19 +4,20 @@ from password_checker import PasswordChecker
 # Defining main, calls class PasswordChecker
 # Input validation to make sure string is not emptys
 
+
 def main():
     print("Hello, welcome to the password strength tester!\n")
 
     password = input("Enter a password to test its strength:\n")
 
-    if input == '':
-        print('Password cannot be empty')
-                     
-    checker = PasswordChecker(password)
+    if password == "":
+        print("Password cannot be empty\n")
+        return
+    else:
+        checker = PasswordChecker(password)
+        result = checker.check_strength()
+        print(result)
 
-    result = checker.check_strength()
-
-    print(result)
 
 if __name__ == "__main__":
     main()
